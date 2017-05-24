a85251d9cc240ef1612850384999656da0356691
bahi-media <steffen.hebestreit@gmx.net>
2017-05-24 13:15:18 +0200

    include Unity workspace

---
+++ Unity/Assets/Skript/LoadLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+
+    public void LoadScene(string scene)
+    {
+       SceneManager.LoadScene("Assets/Save/"+scene+".unity", LoadSceneMode.Single);  
+    }
+}
