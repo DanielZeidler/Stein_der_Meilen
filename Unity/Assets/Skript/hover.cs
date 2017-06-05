e378745d24bc3e974ca07f7881ff6a2fd97b5709
Metanic <daniel_zeidler@web.de>
2017-06-05 19:37:51 +0200

    minispiel1

---
+++ Unity/Assets/Skript/hover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour {
+
+    public Texture2D defaultTexture;
+    public Texture2D pickupTexture;
+    public CursorMode curMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.SetCursor(defaultTexture, hotSpot, curMode);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(pickupTexture, hotSpot, curMode);
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+}
