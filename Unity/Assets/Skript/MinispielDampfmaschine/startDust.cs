9bf4ec5594d834b3096a709bd1cefde1162d28d1
bahi-media <steffen.hebestreit@gmx.net>
2017-06-13 17:10:59 +0200

    some Interaction added to MinigameDampfmaschine

---
+++ Unity/Assets/Skript/MinispielDampfmaschine/startDust.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startDust : MonoBehaviour {
+
+    public ParticleSystem[] particlesToStart;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "fackel")
+        {
+            Destroy(collision.gameObject);
+            foreach(ParticleSystem psys in particlesToStart)
+            {
+                psys.Play();
+            }
+        }
+    }
+}
