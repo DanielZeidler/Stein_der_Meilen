4087447c8101831fd47e9f61baca762e4b73a603
florianfreier <flospielt@posteo.de>
2017-06-17 17:15:03 +0200

    Minigame Schrift Prototyp

---
+++ Unity/Assets/Skript/MinigameSchrift/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour {
+		
+
+	private int count = 0;
+
+
+	public void addCount() {
+		count += 1;
+	}
+	public int getCount(){
+		return count;
+	}
+
+	public bool won() {
+		if (count == 6) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Unity/Assets/Skript/MinigameSchrift/schriftDropzone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class schriftDropzone : MonoBehaviour, IDropHandler
+{
+	void Start() {
+		font = Resources.Load<Font>("Fonts/Hieroglify");
+	}
+
+	public Counter counter;
+		
+	Text text;
+
+	Font font;
+
+
+	public void OnDrop (PointerEventData eventData)
+	{
+		if (eventData.pointerDrag.name == gameObject.name)
+		{
+			eventData.pointerDrag.gameObject.SetActive (false);
+			text = gameObject.GetComponentInChildren<Text> ();
+			text.font = font;
+			counter.addCount();
+			print (counter.getCount());
+		}
+		if (counter.won()) {
+			// Win Game
+			print("You Won");
+		}
+	}
+}
