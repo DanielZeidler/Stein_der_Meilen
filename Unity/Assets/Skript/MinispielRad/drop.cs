6ba0ba8d872276fb4f8a3ef63fb63a8da156da70
Metanic <daniel_zeidler@web.de>
2017-06-23 19:34:42 +0200

    MinispielRad anfang

---
+++ Unity/Assets/Skript/MinispielRad/drop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class drop : MonoBehaviour, IDropHandler
+{
+    public bool kiste;
+    public GameObject wirdsichtbar;
+    public GameObject wirdzerstört;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("test");
+        if(eventData.pointerDrag.name == "wagenohneKiste")
+        {
+            if(!kiste)
+            {
+                wirdsichtbar.SetActive(true);
+                wirdzerstört.SetActive(false);
+            }
+        }
+
+        if (eventData.pointerDrag.name == "wagenmitkiste")
+        {
+            if (kiste)
+            {
+                wirdsichtbar.SetActive(true);
+                wirdzerstört.SetActive(false);
+            }
+        }
+    }
+
+}
--- Unity/Assets/Skript/MinispielRad/onclick.cs
+++ Unity/Assets/Skript/MinispielRad/onclick.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class onclick : MonoBehaviour {
+public class onclick : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+
+    }
 }
