b97e84601cfef3fc9ec29e0101e58548d50c001f
bahi-media <steffen.hebestreit@gmx.net>
2017-06-26 01:00:37 +0200

    finished gameLogic MinispielBuchdruck

---
+++ Unity/Assets/Skript/MinispielBuchdruck/BuchdruckController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuchdruckController : MonoBehaviour {
+
+    public bool boolletter1;
+    public bool boolletter2;
+    public bool boolletter3;
+    public bool boolletter4;
+    public bool boolletter5;
+    public bool boolletter6;
+    public bool boolletter7;
+    public bool boolletter8;
+    public bool boolletter9;
+    public bool boolletter10;
+    public bool boolletter11;
+    public bool boolletter12;
+    public bool boolletter13;
+    public bool boolletter14;
+    public bool boolletter15;
+    public bool boolletter16;
+
+    private ParticleSystem firework;
+    private bool finish;
+    // Use this for initialization
+    void Start () {
+        firework = GameObject.Find("Fireworks").GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       if(!finish && boolletter1 && boolletter2 && boolletter3 && boolletter4 && boolletter5 && boolletter6 && boolletter7 && boolletter8 && boolletter9 && boolletter10 && boolletter11 && boolletter12 && boolletter13 && boolletter14 && boolletter15 && boolletter16)
+        {
+            firework.Play();
+            finish = true;
+        }
+    }
+}
---
+++ Unity/Assets/Skript/MinispielBuchdruck/collisionStayTrigger.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionStayTrigger : MonoBehaviour {
+
+    private BuchdruckController buchdruckController;
+
+    private void Awake()
+    {
+        buchdruckController = GameObject.Find("Main Camera").GetComponent<BuchdruckController>();
+    }
+    
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (gameObject.GetComponent<letterContainer>().letter == collision.GetComponent<letterContainer>().letter)
+        {
+            string name = gameObject.name;
+            string number = name.Replace("ColliderLetter", "");
+            int num = Int32.Parse(number);
+
+            if(num == 1)
+            {
+                buchdruckController.boolletter1 = true;
+            }
+            else if(num == 2)
+            {
+                buchdruckController.boolletter2 = true;
+            }
+            else if (num == 3)
+            {
+                buchdruckController.boolletter3 = true;
+            }
+            else if (num == 4)
+            {
+                buchdruckController.boolletter4 = true;
+            }
+            else if (num == 5)
+            {
+                buchdruckController.boolletter5 = true;
+            }
+            else if (num == 6)
+            {
+                buchdruckController.boolletter6 = true;
+            }
+            else if (num == 7)
+            {
+                buchdruckController.boolletter7 = true;
+            }
+            else if (num == 8)
+            {
+                buchdruckController.boolletter8 = true;
+            }
+            else if (num == 9)
+            {
+                buchdruckController.boolletter9 = true;
+            }
+            else if (num == 10)
+            {
+                buchdruckController.boolletter10 = true;
+            }
+            else if (num == 11)
+            {
+                buchdruckController.boolletter11 = true;
+            }
+            else if (num == 12)
+            {
+                buchdruckController.boolletter12 = true;
+            }
+            else if (num == 13)
+            {
+                buchdruckController.boolletter13 = true;
+            }
+            else if (num == 14)
+            {
+                buchdruckController.boolletter14 = true;
+            }
+            else if (num == 15)
+            {
+                buchdruckController.boolletter15 = true;
+            }
+            else if (num == 16)
+            {
+                buchdruckController.boolletter16 = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (gameObject.GetComponent<letterContainer>().letter == collision.GetComponent<letterContainer>().letter)
+        {
+            string name = gameObject.name;
+            string number = name.Replace("ColliderLetter", "");
+            int num = Int32.Parse(number);
+
+            if (num == 1)
+            {
+                buchdruckController.boolletter1 = false;
+            }
+            else if (num == 2)
+            {
+                buchdruckController.boolletter2 = false;
+            }
+            else if (num == 3)
+            {
+                buchdruckController.boolletter3 = false;
+            }
+            else if (num == 4)
+            {
+                buchdruckController.boolletter4 = false;
+            }
+            else if (num == 5)
+            {
+                buchdruckController.boolletter5 = false;
+            }
+            else if (num == 6)
+            {
+                buchdruckController.boolletter6 = false;
+            }
+            else if (num == 7)
+            {
+                buchdruckController.boolletter7 = false;
+            }
+            else if (num == 8)
+            {
+                buchdruckController.boolletter8 = false;
+            }
+            else if (num == 9)
+            {
+                buchdruckController.boolletter9 = false;
+            }
+            else if (num == 10)
+            {
+                buchdruckController.boolletter10 = false;
+            }
+            else if (num == 11)
+            {
+                buchdruckController.boolletter11 = false;
+            }
+            else if (num == 12)
+            {
+                buchdruckController.boolletter12 = false;
+            }
+            else if (num == 13)
+            {
+                buchdruckController.boolletter13 = false;
+            }
+            else if (num == 14)
+            {
+                buchdruckController.boolletter14 = false;
+            }
+            else if (num == 15)
+            {
+                buchdruckController.boolletter15 = false;
+            }
+            else if (num == 16)
+            {
+                buchdruckController.boolletter16 = false;
+            }
+        }
+    }
+
+}
