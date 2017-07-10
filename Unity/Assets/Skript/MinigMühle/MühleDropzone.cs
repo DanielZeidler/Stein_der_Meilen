1a6518f83519c07f261861d716dc1746bdf36d47
Metanic <daniel_zeidler@web.de>
2017-07-10 03:10:13 +0200

    fertig machen

--- Unity/Assets/Skript/MinigMühle/MühleDropzone.cs
+++ Unity/Assets/Skript/MinigMühle/MühleDropzone.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MühleDropzone : MonoBehaviour, IDropHandler{
+public class MühleDropzone : MonoBehaviour, IDropHandler
+{
+    public GameObject image;
+    public CounterMuehle counter;
 
 
-	public GameObject image;
-	public CounterMuehle counter;
 
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag.name == gameObject.name)
+        {
+            eventData.pointerDrag.gameObject.SetActive(false);
+            image.SetActive(true);
+            counter.addCount();
+        }
 
+        if (counter.won())
+        {
+           
+        }
+    }
 
-	public void OnDrop (PointerEventData eventData)
-	{
-		
-		print (eventData.pointerDrag.name);
-		print ("dropped on");
-		print (gameObject.name);
-
-		if (eventData.pointerDrag.name == gameObject.name)
-		{
-			print ("match");
-			print ("----------");
-			eventData.pointerDrag.gameObject.SetActive (false);
-			image.SetActive (true);
-			counter.addCount ();
-		}
-
-		if (counter.won ()) {
-			// WON THE GAME
-			print("won the Muehle Game");
-
-
-			// TODO Implement Game Won
-
-		}
-	}
 }
+
